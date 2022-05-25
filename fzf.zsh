40f3597fba0c0bada04ed7be3fcc4b7ec96c66ad
Adam C <2928943+kabads@users.noreply.github.com>
2022-05-25 15:14:07 +0100

    Checking what os this is

--- fzf.zsh
+++ fzf.zsh
@@ -1,13 +1,19 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+#
+
+machine=$(uname)
+
+if [[ $machine -eq "Darwin" ]]; then
+    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+        export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+        # Key bindings
+        source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+        # Auto completion
+        [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+    fi
+elif [[ $machine -eq "Linux" ]]; then
+    source /usr/share/fzf/key-bindings.zsh
+else
+    echo "Not sure what machine this is"
 fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
