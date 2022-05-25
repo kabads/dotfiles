3a817e33e62713740235661e141d257b6e6da65f
Adam C <2928943+kabads@users.noreply.github.com>
2022-05-25 16:56:27 +0100

    Chaning equality check

--- fzf.zsh
+++ fzf.zsh
@@ -3,7 +3,8 @@
 #
 
 machine=$(uname)
-if [[ $machine -eq "Darwin" ]]; then
+if [[ $machine = "Darwin" ]]; then
+    echo "This is a mac"
     if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
         export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
         # Key bindings
@@ -11,7 +12,8 @@ if [[ $machine -eq "Darwin" ]]; then
         # Auto completion
         [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
     fi
-elif [[ $machine -eq "Linux" ]]; then
+fi
+if [[ $machine = "Linux" ]]; then
     source /usr/share/fzf/key-bindings.zsh
 else
     echo "Not sure what machine this is"
